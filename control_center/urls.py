7ec1a3e0e30fd1f86d8ebc7ee567e4ee94976de1
Sliznyak-tyan <110898@inbox.ru>
2022-04-21 19:33:05 +0200

    add login and logout

--- control_center/urls.py
+++ control_center/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from .views import IndicatorsView
 from .views import StatisticsView
 from .views import schedule_view
+from .views import login_view
+from .views import logout_view
 
 urlpatterns = [path('indicators/', IndicatorsView.as_view()),
                path('statistics/', StatisticsView.as_view()),
-               path('schedule/', schedule_view)]
+               path('schedule/', schedule_view),
+               path('login/', login_view),
+               path('logout/', logout_view)]
--- control_center/views.py
+++ control_center/views.py
@@ -4,6 +4,10 @@ import requests
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib.auth import logout
 
 from control_center.endpoints import temp_handler
 from control_center.endpoints import humidity_handler
@@ -13,6 +17,8 @@ from control_center.endpoints import recuperator_get_schedule_handler
 
 class IndicatorsView(APIView):
     def get(self, request):
+        if not request.user.is_authenticated:
+            return redirect('/system/login/')
         temperature_data = temp_handler()
         humidity_data = humidity_handler()
         data = {'temperature_data': temperature_data, 'humidity_data': humidity_data}
@@ -21,11 +27,15 @@ class IndicatorsView(APIView):
 
 class StatisticsView(APIView):
     def get(self, request):
+        if not request.user.is_authenticated:
+            return redirect('/system/login/')
         heat_data = heat_handler()
         return render(request, 'html/statistics.html', {'data': heat_data})
 
 
 def schedule_view(request):
+    if not request.user.is_authenticated:
+        return redirect('/system/login/')
     schedule = None
     max_id = 0
     if request.method == 'POST':
@@ -37,3 +47,21 @@ def schedule_view(request):
             print(max_id)
 
     return render(request, 'html/schedule.html', {'schedule': schedule, 'max_id': max_id})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        print(request.POST)
+        username = request.POST['login']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/system/indicators/')
+    else:
+        return render(request, 'html/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/system/login/')
